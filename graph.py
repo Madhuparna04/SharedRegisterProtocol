f98081ad9016c1b6e675cb2330df9b54ce790b63
rsingh76 <rahulksingh271@gmail.com>
2023-02-27 19:23:55 -0600

    pushing the latency-throughput plot of SharedRegisters

--- graph.py
+++ graph.py
@@ -8,13 +8,13 @@ def get_experiment(num_clients, rw):
     lat_graph = []
     cumu_time = 0
     for i in range(num_clients):
-        file_name = "results/" + str(rw) + "/latency_" + str(i) + "_" + str(num_clients) + ".txt"
+        file_name = "sharedresults_results/" + str(rw) + "/latency_" + str(i) + "_" + str(num_clients) + ".txt"
         file = open(file_name, "r")
         latency = []
         for l in file.readlines():
             val = int(l)
             latency.append(val)
-        total_time = latency[-1]
+        total_time = sum(latency[:-1])
         cumu_time = max(cumu_time, total_time)
         latency = latency[0:len(latency)-1]
         avg_latency = sum(latency)/len(latency)
@@ -22,19 +22,26 @@ def get_experiment(num_clients, rw):
         max_latency = max(latency)
         percentile =  np.percentile(latency, 99)
     throughput = (total_ops*1000000)/cumu_time
-    th_graph.append(throughput)
-    lat_graph.append(avg_latency/1000000)
-    print(throughput)
-    return th_graph, lat_graph
+    # th_graph.append(throughput)
+    # lat_graph.append(avg_latency/1000000)
+    print("## ", num_clients, "     ", cumu_time)
+    return throughput, avg_latency/1000000
 
 experiment_nodes = [1,2,4,8,16,32]
 experiment_rw = [100,50,0]
-
 plt.xlabel("Throughput (No. of Operations per second)")
 plt.ylabel("Avg Latency (seconds)")
+
 for rw in experiment_rw:
+    th_graph = []
+    lat_graph = []
     for exp in experiment_nodes:
-        th_graph, lat_graph = get_experiment(exp, rw)
-        plt.plot(th_graph, lat_graph, label=str(rw) + "% Reads, " + str(100-rw) + "% Writes")
+        th, lat = get_experiment(exp, rw)
+        th_graph.append(th)
+        lat_graph.append(lat)
+    plt.plot(th_graph, lat_graph, label=str(rw) + "% Reads, " + str(100-rw) + "% Writes")
+
 plt.legend(loc="upper left")
+# plt.show()
+plt.savefig('latency_throughput.pdf')
 plt.savefig("latency_throughput.png")
